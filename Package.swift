9c3700bdb0b50b4da770efe370f906f2f6de6e31
Marcel Borsten <marcel@impart.nl>
2019-09-23 09:54:20 +0200

    Added SwiftPM file

---
+++ Package.swift
@@ -0,0 +1,24 @@
+//
+//  Package.swift
+//  SpreadsheetView
+//
+//  Created by Marcel Borsten on 23/09/2019.
+//  Copyright © 2019 Kishikawa Katsumi. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "SpreadsheetView",
+    products: [
+        .library(
+            name: "SpreadsheetView",
+            targets: ["SpreadsheetView"])
+    ],
+    targets: [
+        .target(
+            name: "SpreadsheetView",
+            path: "Source")
+    ],
+    swiftLanguageVersions: [.v4, .v5]
+)
