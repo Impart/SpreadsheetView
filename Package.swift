673651c8f5d849a3e94cb22910702d745b80442b
Marcel Borsten <marcel@impart.nl>
2019-09-23 10:00:09 +0200

    Fixes

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     targets: [
         .target(
             name: "SpreadsheetView",
-            path: "Source")
+            path: "Sources")
     ],
     swiftLanguageVersions: [.v4, .v5]
 )
